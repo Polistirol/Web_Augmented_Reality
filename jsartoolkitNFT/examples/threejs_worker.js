3594bf7adfa7235388299a73dc05ec9c79d4cebd
Polistirol <65390018+Polistirol@users.noreply.github.com>
2020-11-13 12:16:57 +0100

    Update threejs_worker.js

--- jsartoolkitNFT/examples/threejs_worker.js
+++ jsartoolkitNFT/examples/threejs_worker.js
@@ -175,7 +175,7 @@ function start(container, marker, video, input_width, input_height, canvas_draw,
 
         worker = new Worker('../js/artoolkitNFT.worker.js');
 
-        worker.postMessage({ type: "load", pw: pw, ph: ph, camera_para: camera_para, marker: '../' + marker.url });
+        worker.postMessage({ type: "load", pw: pw, ph: ph, camera_para: camera_para, marker:  marker.url });
 
         worker.onmessage = function (ev) {
             var msg = ev.data;
